de74b071dcc8f4956cdd43b7a2872a396b9f25c5
Miguel Medina Cantos <75499798+MiguelMC-UNEX@users.noreply.github.com>
2024-09-03 13:36:18 +0200

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,25 @@
+// JavaScript para detectar si los elementos están en el viewport y añadir la clase visible
+document.addEventListener("DOMContentLoaded", function() {
+    const elements = document.querySelectorAll('.fade-in');
+
+    const isVisible = (el) => {
+        const rect = el.getBoundingClientRect();
+        return (
+            rect.top >= 0 &&
+            rect.left >= 0 &&
+            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+            rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+        );
+    };
+
+    const checkVisibility = () => {
+        elements.forEach(element => {
+            if (isVisible(element)) {
+                element.classList.add('visible');
+            }
+        });
+    };
+
+    window.addEventListener('scroll', checkVisibility);
+    checkVisibility(); // Activar la animación al cargar la página
+});
